2ab1d5629ebfa4ffdc3c625f5afb2bdf41005595
prsclasnts <prsclalim@outlook.com>
2024-03-21 22:39:29 -0300

    Adicionando os templates base e home, e trazendo os dados do banco

--- news/main/views.py
+++ news/main/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import News, Category
 
-# Create your views here.
+def home(request):
+    first_news=News.objects.first()
+    three_news=News.objects.all()[1:3]
+    three_categories=Category.objects.all()[0:3]
+    return render(request, 'home.html', {
+        'first_news': first_news,
+        'three_news': three_news,
+        'three_categories': three_categories
+    })
